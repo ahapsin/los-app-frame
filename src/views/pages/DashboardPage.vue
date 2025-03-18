fc29970b482d5afc3e0b6a9036f78494a8dc61f4
ahapsin <ahapsin02@gmail.com>
2025-03-18 10:28:30 +0700

    msurvey

--- src/views/pages/DashboardPage.vue
+++ src/views/pages/DashboardPage.vue
@@ -3,22 +3,19 @@
         Hi,<b>{{ me.me.nama }} 👋</b>
     </n-card>
 
-
-    <div class="grid grid-flow-dense md:grid-cols-3 gap-2 mt-2">
-        <!--    <n-card v-for="dashCard in me.me.accessMenu" :title="dashCard">-->
-        <!--      <div>-->
-        <!--        {{ dashCard }}-->
-        <!--      </div>-->
-        <!--    </n-card>-->
-        <!--    {{ findAccessMenu('Oas') ? 'a' : 'b' }}-->
-        <OrderBoard v-if="findAccessMenu('Order')" />
-        <PaymentBoard v-if="findAccessMenu('Pembayaran')" />
-        <JaminanBoard v-if="findAccessMenu('jaminan')" />
+  <MenuContainer class="mt-2"/>
+<n-scrollbar x-scrollable class="h-fit ">
+    <div class="grid grid-flow-col md:grid-cols-3 gap-2 mt-2">
+        <OrderBoard v-if="findAccessMenu('Order')" class="w-screen shadow"/>
+        <PaymentBoard v-if="findAccessMenu('Pembayaran')" class="w-screen shadow"/>
+        <JaminanBoard v-if="findAccessMenu('jaminan')" class="w-screen shadow"/>
     </div>
+</n-scrollbar>
 </template>
 <script setup>
 import {defineAsyncComponent} from 'vue'
 import _ from "lodash";
+import MenuContainer from "./../../components/organism/MenuContainer.vue";
 import {useMeStore} from "../../stores/me.js";
 
 
