5ff04d1e416a89faf7a5e6ea171d43cdda716956
ahapsin <121087803+ahapsin@users.noreply.github.com>
2025-04-19 11:31:56 +0700

    asa

--- src/views/pages/task/AddSurveyModPage.vue
+++ src/views/pages/task/AddSurveyModPage.vue
@@ -194,7 +194,7 @@
                 <div>
                     <select-state-region v-model:provinsi="pelanggan.provinsi" v-model:kota="pelanggan.kota"
                         v-model:kecamatan="pelanggan.kecamatan" v-model:desa="pelanggan.kelurahan"
-                        v-model:kodepos="pelanggan.kode_pos" />
+                        v-model:kodepos="pelanggan.kode_pos"/>
                 </div>
                 <n-divider title-placement="left"> UPLOAD DOKUMEN IDENTITAS</n-divider>
 
