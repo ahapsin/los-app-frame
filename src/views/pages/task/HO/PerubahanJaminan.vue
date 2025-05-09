d5951e3499263f1cb4b5df8c5897ee35dc421339
ahapsin <ahapsin02@gmail.com>
2025-05-09 13:44:09 +0700

    jaminan

--- src/views/pages/task/HO/PerubahanJaminan.vue
+++ src/views/pages/task/HO/PerubahanJaminan.vue
@@ -1,7 +1,7 @@
 <template>
   <n-card size="small" :segmented="true">
     <template #header>Perubahan Jaminan</template>
-    <template #header-extra>
+    <!-- <template #header-extra>
       <n-space>
         <n-badge :value="dataPending.length" :max="99" type="warning">
           <n-button v-show="!searchField" strong secondary type="warning" @click="dataPayment = dataPending">
@@ -14,30 +14,123 @@
           </n-button>
         </n-badge>
       </n-space>
-    </template>
+    </template> -->
     <div>
       <n-data-table striped size="small" :row-key="(row) => row.loan_number" :columns="columns" :data="dataPayment"
-        :max-height="300" class="pb-2" :pagination="pagination" />
+        class="pb-2" :pagination="pagination" :loading="loadDataPayment" />
     </div>
   </n-card>
-  
+  <n-modal v-model:show="showModal">
+    <n-card title="Konfirmasi perubahan data jaminan" class="w-1/2" :segmented="true" size="small">
+      <template #header-extra>
+        <n-space>
+          <n-button type="error" secondary size="small" @click="handleTolak(bodyModal.id)">Tolak</n-button>
+          <n-button type="success" size="small" @click="handleSetuju(bodyModal.id)">Setuju</n-button>
+
+        </n-space>
+      </template>
+      <div class="grid grid-cols-3 gap-4 p-2">
+        <div class="flex flex-col">
+          <small class="text-reg">Tanggal</small>
+          <n-text strong class="text-md border-b"> {{ bodyModal.tanggal }}</n-text>
+        </div>
+        <div class="flex flex-col">
+          <small class="text-reg">Pemohon</small>
+          <n-text strong class="text-md border-b"> {{ bodyModal.dari }}</n-text>
+        </div>
+        <div class="flex flex-col">
+          <small class="text-reg">Keterangan</small>
+          <n-text strong class="text-md border-b"> {{ bodyModal.keterangan ? bodyModal.keterangan : 'N/A' }}</n-text>
+        </div>
+      </div>
+      <div class="gap-2 flex flex-col">
+        <div class="border border-red-500 rounded-lg overflow-clip">
+          <div class="border-b p-2 border-red-500 bg-red-50 font-bold">Data Lama</div>
+          <div class="p-2">
+            <div class="grid grid-cols-3 gap-4 p-2">
+              <div class="flex flex-col">
+                <small class="text-reg">Atas Nama</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.atas_nama }}</n-text>
+              </div>
+              <div class="flex flex-col">
+                <small class="text-reg">No Polisi</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.no_polisi }}</n-text>
+              </div>
+              <div class="flex flex-col">
+                <small class="text-reg">No Rangka</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.no_rangka }}</n-text>
+              </div>
+              <div class="flex flex-col col-span-3">
+                <small class="text-reg">Alamat BPKB</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.alamat_bpkb }}</n-text>
+              </div>
+              <div class="flex flex-col">
+                <small class="text-reg">No STNK</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.no_stnk }}</n-text>
+              </div>
+              <div class="flex flex-col">
+                <small class="text-reg">No Faktur</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.no_faktur }}</n-text>
+              </div>
+              <div class="flex flex-col">
+                <small class="text-reg">No Faktur</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.tgl_stnk }}</n-text>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="border border-green-500 rounded-lg overflow-clip">
+          <div class="border-b p-2 border-green-500 bg-green-50 font-bold">Data Baru</div>
+          <div class="p-2">
+            <div class="grid grid-cols-3 gap-4 p-2">
+              <div class="flex flex-col">
+                <small class="text-reg">Atas Nama</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.after.atas_nama }}</n-text>
+              </div>
+              <div class="flex flex-col">
+                <small class="text-reg">No Polisi</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.after.no_polisi }}</n-text>
+              </div>
+              <div class="flex flex-col">
+                <small class="text-reg">No Rangka</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.after.no_rangka }}</n-text>
+              </div>
+              <div class="flex flex-col col-span-3">
+                <small class="text-reg">Alamat BPKB</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.after.alamat_bpkb }}</n-text>
+              </div>
+              <div class="flex flex-col">
+                <small class="text-reg">No STNK</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.after.no_stnk }}</n-text>
+              </div>
+              <div class="flex flex-col">
+                <small class="text-reg">No Faktur</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.after.no_faktur }}</n-text>
+              </div>
+              <div class="flex flex-col">
+                <small class="text-reg">No Faktur</small>
+                <n-text strong class="text-md border-b"> {{ bodyModal.after.tgl_stnk }}</n-text>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </n-card>
+  </n-modal>
 </template>
 <script setup>
-import {
-  AccessTimeRound as pendingIcon
-} from "@vicons/material";
 import _ from "lodash";
 import {
   NBadge,
   NButton,
-  NIcon,
-  NTag,
+  useDialog,
   useLoadingBar,
   useMessage
 } from "naive-ui";
 import { computed, h, onMounted, reactive, ref } from "vue";
 import { useApi } from "../../../../helpers/axios";
-import router from "../../../../router";
+
+const dialog = useDialog();
 const checkedRowCredit = ref([]);
 const pagination = ref({ pageSize: 10 });
 const totalPay = computed(() => {
@@ -73,64 +166,24 @@ const pageData = reactive({
 const createColumns = () => {
   return [
     {
-      title: "cabang",
-      sorter: "default",
-      key: "cabang",
-    },
-    {
-      title: "no kontrak",
-      sorter: "default",
-      key: "no_fasilitas",
-    },
-    {
-      title: "no transaksi",
-      sorter: "default",
-      ellipsis: {
-        tooltip: true
-      },
-      key: "no_transaksi",
-    },
-    {
       title: "tanggal",
-      key: "tgl_transaksi",
       sorter: "default",
+      key: "tanggal",
     },
     {
-      title: "metode",
-      key: "payment_method",
+      title: "dari",
       sorter: "default",
+      key: "dari",
     },
     {
-      title: "atas nama",
-      key: "nama",
+      title: "No Jaminan",
       sorter: "default",
+      key: "no_bpkb",
     },
     {
-      title: "nominal",
-      key: "total_bayar",
-      align: 'right',
-      render(row) {
-        return h("div", row.total_bayar.toLocaleString("US"));
-      },
+      title: "Atas Nama",
       sorter: "default",
-    },
-    {
-      title: "status",
-      key: "STATUS",
-      render(row) {
-        return h(
-          NTag,
-          {
-            type: statusTag(row.STATUS),
-            onClick: () => {
-              handleAction(row);
-            },
-          },
-          {
-            default: () => row.STATUS,
-          }
-        );
-      },
+      key: "atas_nama",
     },
     {
       align: "right",
@@ -170,72 +223,67 @@ const handleAction = (e) => {
   showModal.value = true;
   bodyModal.value = e;
 };
-
-const statusTag = (e) => {
-  if (e === "CANCEL") {
-    return "error";
-  }
-  if (e === "PAID") {
-    return "success";
-  }
-  if (e === "PENDING") {
-    return "warning";
-  }
-}
 const columns = createColumns();
-let userToken = localStorage.getItem("token");
-const keterangan = ref();
-const loadingConf = ref(false);
-const handlePositiveClick = async (e) => {
-  loadingConf.value = true;
-  const bodyPost = {
-    no_invoice: e,
-    flag: "yes",
-    keterangan: keterangan.value,
-    struktur: bodyModal.value.struktur,
-  };
-  const response = await useApi({
-    method: "POST",
-    api: `payment_approval`,
-    data: bodyPost,
-    token: userToken,
+const handleSetuju = (e) => {
+  dialog.warning({
+    title: 'Setuju ?',
+    content: 'Yakin setujui perubahan jaminan?',
+    positiveText: 'Ya',
+    negativeText: 'Tidak',
+    draggable: true,
+    onPositiveClick: async () => {
+      const response = await useApi({
+        method: "POST",
+        data: {
+          request_id: e,
+          flag_approval: 'yes'
+        },
+        api: "collateral_approval",
+        token: localStorage.getItem('token'),
+      });
+      if (response.ok) {
+        message.info('SELESAI');
+        showModal.value=false;
+        getDataPayment();
+      }else{
+         showModal.value=false;
+      }
+    },
+    onNegativeClick: () => {
+       
+    }
   });
-  if (!response.ok) {
-    loadingConf.value = true;
-    message.error("konfirmasi gagal");
-  } else {
-    loadingConf.value = false;
-    message.success("konfirmasi berhasil");
-    getDataPayment();
-    showModal.value = false;
-    router.push({ name: "pending transfer" });
-  }
-};
-const handleNegativeClick = async (e) => {
-  loadingConf.value = true;
-  const bodyPost = {
-    no_invoice: e,
-    flag: "no",
-    keterangan: keterangan.value,
-    struktur: bodyModal.value.struktur,
-  };
-  const response = await useApi({
-    method: "POST",
-    api: `payment_approval`,
-    data: bodyPost,
-    token: userToken,
+}
+const handleTolak = (e) => {
+  dialog.warning({
+    title: 'Tolak ?',
+    content: 'Yakin tolak perubahan jaminan?',
+    positiveText: 'Ya',
+    negativeText: 'Tidak',
+    draggable: true,
+    onPositiveClick: async () => {
+      const response = await useApi({
+        method: "POST",
+        data: {
+          request_id: e,
+          flag_approval: 'no'
+        },
+        api: "collateral_approval",
+        token: localStorage.getItem('token'),
+      });
+      if (response.ok) {
+        message.info('SELESAI');
+        getDataPayment();
+        showModal.value=false;
+      }else{
+         showModal.value=false;
+      }
+    },
+    onNegativeClick: () => {
+       
+    }
   });
-  if (!response.ok) {
-    loadingConf.value = true;
-    message.error("konfirmasi gagal");
-  } else {
-    loadingConf.value = false;
-    message.success("konfirmasi berhasil");
-    getDataPayment();
-    showModal.value = false;
-    router.push({ name: "pending transfer" });
-  }
-};
+}
 const dataPayment = ref([]);
 const loadDataPayment = ref(false);
 const dataPending = ref([]);
