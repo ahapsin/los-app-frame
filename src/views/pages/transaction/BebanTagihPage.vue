529b6601dba5ca96d8f85ddaeea6a5d260bcb5a2
ahapsin <ahapsin02@gmail.com>
2025-09-19 14:25:14 +0700

    beban tagih

--- src/views/pages/transaction/BebanTagihPage.vue
+++ src/views/pages/transaction/BebanTagihPage.vue
@@ -1,33 +1,46 @@
 <template>
     <n-card title="Beban Tagih" :segmented="true" size="small">
         <div>
+            <n-alert v-if="hasActiveFilters" type="warning" :show-icon="false" class="mb-4 filter-status"
+                title="Filter Aktif">
+                <div class="filter-status-list flex flex-col gap-y-2">
+                    <div v-for="(values, key) in activeFilters" :key="key" class="filter-group">
+                        <span class="filter-key">{{ key }}:</span>
+                        <n-tag v-for="value in values" :key="value" closable round size="small"
+                            :on-close="() => removeFilter(key, value)" class="filter-tag">
+                            {{ value }}
+                        </n-tag>
+                    </div>
+                </div>
+            </n-alert>
             <n-space vertical :size="12">
-                <n-input type="text" placeholder="nyari apa ?" v-model:value="boxSearch" v-if="!ctrDownload"
-                    @blur="searchData" />
-                <n-data-table :columns="columnBebanTagih" :data="dataList" :checked-row-keys="checkedRowKeys"
-                    :row-key="(row) => row" @update:checked-row-keys="handleCheck" :max-height="350"
-                    :loading="isLoading" size="small"  :pagination="{pageSize:10}"/>
+                <n-data-table :columns="columnBebanTagih" :data="filteredDataList" :filter-value="filterValue"
+                    @update:filters="onFilterChange" :checked-row-keys="checkedRowKeys" :row-key="(row) => row"
+                    @update:checked-row-keys="handleCheck" :loading="isLoading" size="small"
+                    :pagination="{ pageSize: 10 }" />
             </n-space>
         </div>
     </n-card>
+
     <n-float-button :right="40" :bottom="40" type="primary" v-if="checkedRowKeys.length > 0"
         @click="modalAssign = true">
         <n-badge :value="checkedRowKeys.length" :offset="[6, -8]">
             <v-icon name="bi-plus-lg" />
         </n-badge>
-
     </n-float-button>
+
     <n-modal v-model:show="modalAssign" :mask-closable="false">
         <n-card class="w-1/4">
             <n-space>
                 <n-button class="mb-2" round type="info" secondary @click="detailSelectedModal = true">{{
-                    checkedRowKeys.length }} data dipilih</n-button>
+                    checkedRowKeys.length
+                    }} data dipilih</n-button>
             </n-space>
 
             <n-space vertical>
                 <n-form-item label="petugas">
                     <n-select v-model:value="assignTo" placeholder="pilih petugas"
-                        :options="_.filter(dataUser, { 'cabang_nama': me.me.cabang_nama })" value-field="username"
+                        :options="_.filter(dataUser, { cabang_nama: me.me.cabang_nama })" value-field="username"
                         label-field="nama" filterable />
                 </n-form-item>
                 <n-space>
@@ -38,176 +51,139 @@
             </n-space>
         </n-card>
     </n-modal>
+
     <n-modal v-model:show="detailSelectedModal">
         <n-card>
-            <n-data-table :columns="selectBebanTagih" :data="checkedRowKeys" :pagination="pagination"
-                :row-key="(row) => row" @update:checked-row-keys="handleCheck" :max-height="350" :loading="isLoading" />
+            <n-data-table :columns="selectBebanTagih" :data="checkedRowKeys" :pagination="pagination" :row-key="rowKey"
+                @update:checked-row-keys="handleCheck" :max-height="350" :loading="isLoading" />
         </n-card>
     </n-modal>
 </template>
+
 <script setup>
+import { NTag } from 'naive-ui';
+import { ref, reactive, computed, onMounted } from "vue";
 import { useLoadingBar, useMessage } from "naive-ui";
-import { onMounted, ref } from "vue";
 import { useApi } from "../../../helpers/axios.js";
 import { useMeStore } from "../../../stores/me";
 import _ from "lodash";
 
-
 const me = useMeStore();
 const message = useMessage();
+const loadingBar = useLoadingBar();
 
 const modalAssign = ref(false);
-const assignTo = ref();
+const assignTo = ref(null);
 const detailSelectedModal = ref(false);
 
-
-const selectedBranch = ref();
-
-const rangeDate = ref();
-let messageReactive = null;
-const loadingBar = useLoadingBar();
-const handleSubmit = () => {
-    let a = {
-        dari: rangeDate.value,
-        cabang_id: selectedBranch.value?.id ? selectedBranch.value.id : me.me.cabang_id,
-    }
-    messageReactive = message.loading('memuat data listing beban', { duration: 0 });
-    grabListBan(a);
-}
-const dataListBan = ref([]);
-const loadingData = ref(false);
-const timer = ref(60);
-const disbaledButton = ref(false);
+const dataUser = ref([]);
+const dataList = ref([]);
+const isLoading = ref(false);
+const checkedRowKeys = ref([]);
+const boxSearch = ref("");
 const ctrDownload = ref(true);
-const grabListBan = async (e) => {
 
-    loadingData.value = true;
-    let userToken = localStorage.getItem("token");
-    const response = await useApi({
-        method: "POST",
-        api: "listBanTest",
-        data: e,
-        token: userToken,
+const rowKey = (row) => row["NO KONTRAK"];
+
+const filterValue = reactive({
+    NBOT: [],
+    KECAMATAN: [],
+    SURVEYOR: [],
+    "SURVEYOR STATUS": [],
+});
+
+function onFilterChange(newFilter) {
+    Object.keys(newFilter).forEach((key) => {
+        filterValue[key] = newFilter[key] || [];
     });
-    if (!response.ok) {
-        messageReactive.destroy();
-        disbaledButton.value = true;
-        var interval;
-        interval = setInterval(() => {
-            if (timer.value > 0) {
-                timer.value--
-            } else {
-                clearInterval(interval);
-                timer.value = 60;
-                handleSubmit();
-            }
-        }, 1000);
-        messageReactive = message.loading(() => (`data listban sedang sibuk mencoba ulang dalam ${timer.value} s`), { duration: 60000 });
-    } else {
-        messageReactive.destroy();
-        messageReactive = null;
-        disbaledButton.value = false;
-        dataListBan.value = response.data;
-        loadingData.value = false;
-        ctrDownload.value = false;
-    }
-
 }
 
-const columnBebanTagih = [
-    {
-        type: "selection",
-    },
+const columnBebanTagih = reactive([
+    { type: "selection" },
     {
         title: "NO SURAT",
         key: "NO SURAT",
-        width: '150',
-        sorter: 'default'
+        width: 150,
+        sorter: "default",
     },
     {
         title: "NO KONTRAK",
         key: "NO KONTRAK",
-        width: '150',
-        sorter: 'default'
+        width: 150,
+        sorter: "default",
     },
     {
         title: "NAMA KOMSUMEN",
         key: "NAMA PELANGGAN",
-        width: '200',
-        sorter: 'default'
+        width: 200,
+        sorter: "default",
     },
     {
         title: "NAMA PIC",
         key: "PIC",
-        width: '200',
-        sorter: 'default'
+        width: 200,
+        sorter: "default",
     },
     {
         title: "CYCLE AWAL",
         key: "CYCLE AWAL",
-        sorter: 'default'
+        sorter: "default",
     },
     {
         title: "NBOT",
-        key: "name",
-        sorter: 'default'
+        key: "NBOT",
+        sorter: "default",
+        filter: true,
+        filterMultiple: true,
+        filterOptions: [],
     },
     {
         title: "DESA",
         key: "KELURAHAN",
-        sorter: 'default'
+        sorter: "default",
     },
     {
         title: "KEC",
         key: "KECAMATAN",
-        sorter: 'default'
+        sorter: "default",
+        filter: true,
+        filterMultiple: true,
+        filterOptions: [],
     },
     {
         title: "MCF",
         key: "SURVEYOR",
-        sorter: 'default'
+        sorter: "default",
+        filter: true,
+        filterMultiple: true,
+        filterOptions: [],
     },
-];
-const selectBebanTagih = [
+    {
+        title: "KET",
+        key: "SURVEYOR STATUS",
+        sorter: "default",
+        filter: true,
+        filterMultiple: true,
+        filterOptions: [],
+    },
+]);
 
-    {
-        title: "NO KONTRAK",
-        key: "NO KONTRAK",
-        width: '150'
-    },
-    {
-        title: "NAMA KOMSUMEN",
-        key: "NAMA PELANGGAN",
-        width: '200'
-    },
-    {
-        title: "NAMA PIC",
-        key: "name",
-        width: '200',
-    },
-    {
-        title: "CYCLE AWAL",
-        key: "CYCLE AWAL",
-    },
-    {
-        title: "NBOT",
-        key: "name"
-    },
-    {
-        title: "DESA",
-        key: "KELURAHAN"
-    },
-    {
-        title: "KEC",
-        key: "KECAMATAN"
-    },
-    {
-        title: "MCF",
-        key: "SURVEYOR"
-    },
-];
+// Fungsi untuk menangani pencarian, ini akan memfilter dataList berdasarkan boxSearch
+const searchData = () => {
+    if (boxSearch.value) {
+        const searchTerm = boxSearch.value.toLowerCase();
+        dataList.value = dataList.value.filter((row) => {
+            // Ubah semua nilai di setiap baris menjadi string, lalu cek apakah mengandung searchTerm
+            return Object.values(row).some((val) =>
+                String(val).toLowerCase().includes(searchTerm)
+            );
+        });
+    } else {
+        // Jika boxSearch kosong, muat ulang data untuk mengembalikan data asli
+        getList();
+    }
+};
 
-const dataUser = ref([]);
 const getData = async () => {
     let userToken = localStorage.getItem("token");
     const response = await useApi({
@@ -216,15 +192,13 @@ const getData = async () => {
         token: userToken,
     });
     if (!response.ok) {
-        console.log(reponse.error);
+        console.error(response.error);
     } else {
         loadingBar.finish();
-        // console.log(response.data.response)
         dataUser.value = response.data.response;
     }
 };
-const dataList = ref([]);
-const isLoading = ref(false);
+
 const getList = async () => {
     isLoading.value = true;
     let userToken = localStorage.getItem("token");
@@ -233,23 +207,41 @@ const getList = async () => {
         api: "tagihan",
         token: userToken,
     });
+
     if (!response.ok) {
         isLoading.value = false;
-        console.log(reponse.error);
+        console.error(response.error);
     } else {
         isLoading.value = false;
         loadingBar.finish();
-        // console.log(response.data.response)
         dataList.value = response.data;
+
+        const uniqueValues = (key) => {
+            return [...new Set(response.data.map((item) => item[key]).filter(Boolean))];
+        };
+
+        const setFilterOptions = (key) => {
+            const col = columnBebanTagih.find((c) => c.key === key);
+            if (col) {
+                col.filterOptions = uniqueValues(key).map((val) => ({
+                    label: val,
+                    value: val,
+                }));
+            }
+        };
+
+        setFilterOptions("NBOT");
+        setFilterOptions("KECAMATAN");
+        setFilterOptions("SURVEYOR");
+        setFilterOptions("SURVEYOR STATUS");
     }
 };
 
-const checkedRowKeys = ref([]);
 const assignTagihan = async () => {
     const bodyPost = {
         user_id: assignTo.value,
-        list_tagihan: checkedRowKeys.value
-    }
+        list_tagihan: checkedRowKeys.value,
+    };
     isLoading.value = true;
     let userToken = localStorage.getItem("token");
     const response = await useApi({
@@ -259,10 +251,10 @@ const assignTagihan = async () => {
         token: userToken,
     });
     if (!response.ok) {
-        console.log(response);
+        console.error(response);
     } else {
         isLoading.value = false;
-        message.success('Berhasil mengirimkan tagihan')
+        message.success("Berhasil mengirimkan tagihan");
         modalAssign.value = false;
         assignTo.value = null;
         checkedRowKeys.value = [];
@@ -270,19 +262,64 @@ const assignTagihan = async () => {
     }
 };
 
-
 function handleCheck(rowKeys) {
     checkedRowKeys.value = rowKeys;
 }
 
-const boxSearch = ref();
+// Tambahkan computed property untuk data yang difilter
+const filteredDataList = computed(() => {
+    let filteredData = dataList.value;
 
+    // Terapkan filter untuk setiap kolom
+    for (const key in filterValue) {
+        if (filterValue[key].length > 0) {
+            filteredData = filteredData.filter((row) =>
+                filterValue[key].includes(row[key])
+            );
+        }
+    }
+
+    // Terapkan filter pencarian global jika ada
+    if (boxSearch.value) {
+        const searchTerm = boxSearch.value.toLowerCase();
+        filteredData = filteredData.filter((row) => {
+            return Object.values(row).some((val) =>
+                String(val).toLowerCase().includes(searchTerm)
+            );
+        });
+    }
+
+    return filteredData;
+});
+// A computed property to get only the filters that have values
+const activeFilters = computed(() => {
+    const active = {};
+    for (const key in filterValue) {
+        if (filterValue[key] && filterValue[key].length > 0) {
+            active[key] = filterValue[key];
+        }
+    }
+    return active;
+});
+
+// Check if there are any active filters to show
+const hasActiveFilters = computed(() => {
+    return Object.keys(activeFilters.value).length > 0;
+});
+
+// Function to remove a single filter tag
+const removeFilter = (key, valueToRemove) => {
+    const values = filterValue[key];
+    if (values) {
+        const index = values.indexOf(valueToRemove);
+        if (index > -1) {
+            values.splice(index, 1);
+        }
+    }
+};
 onMounted(() => {
     loadingBar.finish();
     getData();
     getList();
-}
-)
-    ;
-
+});
 </script>
