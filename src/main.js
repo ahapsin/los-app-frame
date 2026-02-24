fed2e1ae64a12f75707cc6feaa24191bd4d36fe7
ahapsin <ahapsin02@gmail.com>
2026-02-24 11:15:54 +0700

    leaflet.js icon

--- src/main.js
+++ src/main.js
@@ -1,21 +1,11 @@
 import "./style.css";
-import 'leaflet/dist/leaflet.css';
-
-import * as BIIcons from "oh-vue-icons/icons/bi";
-import * as RIcons from "oh-vue-icons/icons/ri";
-
-import { OhVueIcon, addIcons } from "oh-vue-icons";
 
 import App from "./App.vue";
+import { OhVueIcon } from "oh-vue-icons";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPersistedstate from 'pinia-plugin-persistedstate'
 import router from "./router";
-
-const Bi = Object.values({ ...BIIcons });
-const Ri = Object.values({ ...RIcons });
-
-addIcons(...Bi,...Ri);
 
 const app = createApp(App);
 const pinia = createPinia();
