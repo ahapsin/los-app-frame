b27c1f7758d8c10bc1d6bb7f59997c43afcc4fe7
ahapsin <ahapsin02@gmail.com>
2026-02-24 14:20:47 +0700

    main .js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import "./style.css";
 
+import * as BIIcons from "oh-vue-icons/icons/bi";
+import * as RIcons from "oh-vue-icons/icons/ri";
+
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+
 import App from "./App.vue";
-import { OhVueIcon } from "oh-vue-icons";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPersistedstate from 'pinia-plugin-persistedstate'
 import router from "./router";
+
+const Bi = Object.values({ ...BIIcons });
+const Ri = Object.values({ ...RIcons });
+
+addIcons(...Bi,...Ri);
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -13,5 +22,4 @@ pinia.use(piniaPersistedstate);
 app.component("v-icon", OhVueIcon);
 app.use(router);
 app.use(pinia);
-
 app.mount("#app");
